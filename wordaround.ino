e0e5819c56eea7b2c6f641f7d805c62411a8e92b
jaflo <imjaflo@gmail.com>
2015-11-19 09:31:09 -0600

    Added Gabriel's sample file, made changes to handle strings

--- wordaround.ino
+++ wordaround.ino
@@ -1,17 +1,16 @@
 #include <LiquidCrystal.h>
 #include <string.h>
- 
 #include <SPI.h>
 #include <SD.h>
 
 const int chipSelect = 4;
 //String strings[] = {"Gulliver's Travels", "Major Barbara", "Mrs. Dalloway", "Frankenstein", "Fences", "The Turn of the Screw", "Bleak House", "Doctor Faustus", "Catcher in the Rye", "A Tale of Two Cities", "Little Women", "To Kill a Mockingbird", "Oedipus Rex", "Pride and Prejudice", "Anthony and Cleopatra", "As You Like It", "Hamlet", "Henry IV, Parts I and II", "Henry V", "Julius Caesar"};// "King Lear", "Macbeth", "Merchant of Venice", "A Midsummer Night's Dream", "Much Ado About Nothing", "Othello", "Richard III", "Romeo and Juliet", "The Tempest", "Twelfth Night", "Winter’s Tale", "Antigone", "The Grapes of Wrath", "Jude the Obscure", "The Jungle", "Portrait of a Lady", "Rosencrantz and Guildenstern Are Dead", "A Raisin in the Sun", "All the Pretty Horses", "Anna Karenina", "Red Badge of Courage", "As I Lay Dying", "The Color Purple", "The Glass Menagerie", "Native Son", "Song of Solomon", "A Streetcar Named Desire", "The Crucible", "War and Peace", "One Day in the Life of Ivan Denisovich", "Of Mice and Men", "Old Man and the Sea", "All the King’s Men", "Ethan Frome", "The Road", "Lord Jim", "August Wilson", "Madame Bovary", "Medea", "The Merchant of Venice", "Invisible Man", "Wuthering Heights", "Great Expectations", "Heart of Darkness", "King Lear", "Crime and Punishment", "Lord of the Flies", "Jane Eyre", "The Adventures of Huckleberry Finn", "Moby Dick", "The Scarlet Letter", "Their Eyes Were Watching God", "The Awakening", "Catch-22", "Billy Budd", "The Great Gatsby", "Beloved", "Cat’s Cradle", "Slaughterhouse Five", "A Handmaid’s Tale", "A Farewell to Arms", "For Whom the Bell Tolls", "The Sun Also Rises", "The Tempest", "Things Fall Apart", "Who’s Afraid of Virginia Woolf?", "The Sound and the Fury", "A Doll’s House", "An Enemy of the People", "A Modest Proposal", "Death of a Salesman", "A Passage to India", "Candide", "Light in August", "One Day in the Life of Ivan Denisovich", "One Hundred Years of Solitude"};
 char words[10][32];
-char string[10] = {'a', 'r','d', 'u', 'i', 'n', 'o', ' ', 'i', 's'};
-int k = 0;
-String phrase = "";
+int wordIndex = 0;
+char phrase[32];
 int buttonPin = 9;
 int buttonState = 0;
+boolean registeredPress = false;
 LiquidCrystal lcd(6, 7, 5, 8, 3, 2);
 
 void setup() {
@@ -48,6 +47,7 @@ void setup() {
       } else {
         i++;
       }
+      Serial.println(c);
     }
     dataFile.close();
   } else {
@@ -67,11 +67,24 @@ void setup() {
   lcd.print("Done!");
 }
 void loop() {
-  phrase = words[k];
+  int currentLength = 0;
+  lcd.clear();
+  for (int i = 0; i < sizeof(phrase) / sizeof(char); i++) {
+    phrase[i] = '\0';
+    lcd.print(' ');
+  }
+  for (int i = 0; i < sizeof(phrase) / sizeof(char); i++) {
+    if (words[wordIndex][i] == '\0') {
+      break;
+    } else {
+      phrase[i] = words[wordIndex][i];
+      currentLength++;
+    }
+  }
   int line = 0;
-  
-  for (int i = 0; i < sizeof(words) / sizeof(char[32]); i++)  {
-    if (i == 15 && phrase[i + 1] != ' ' && line == 0) {
+  lcd.clear();
+  for (int i = 0; i < currentLength; i++)  {
+    if (i == 15 && phrase[i+1] != ' ' && line == 0) {
       int j = 0;
       for (int k = 15; k > 0; k--)  {
         if (phrase[k] == ' ')  {
@@ -79,7 +92,6 @@ void loop() {
           break;
         }
       }
-
       lcd.setCursor(j, line);
       for (int k = j; k < 16; k++)  {
         lcd.print(" ");
@@ -87,25 +99,25 @@ void loop() {
       i = j;
       line = 1;
       lcd.setCursor(0, line);
-    }
-    else  {
+    } else {
       lcd.print(phrase[i]);
     }
-    Serial.println(i);
+    //Serial.println(phrase);
   }
   // read the state of the pushbutton value:
   buttonState = digitalRead(buttonPin);
 
   // check if the pushbutton is pressed.
   // if it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
+  if (buttonState == HIGH && !registeredPress) {
     // turn LED on:
-    k++;
+    wordIndex++;
+    registeredPress = true;
+  } else {
+    registeredPress = false;
   }
-  else  {
-  }
-  if (k == sizeof(words) / sizeof(char))  {
-    k = 0;
+  if (wordIndex == sizeof(words) / sizeof(char[32]))  {
+    wordIndex = 0;
   }
 
   delay(125);
