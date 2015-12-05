770d64ab064b0ea0876aa44cc690276893c6aff1
jaflo <imjaflo@gmail.com>
2015-12-05 14:39:14 -0600

    I think this is done

--- wordaround.ino
+++ wordaround.ino
@@ -4,27 +4,28 @@
 #include "pitches.h"
 
 // Constants
-unsigned const int pointsToWin = 10;
-unsigned const int roundLength = 10; // in seconds
+const byte pointsToWin = 10;
+const byte roundLength = 10; // in seconds
 
 LiquidCrystal lcd(6, 7, 5, 8, 3, 2);
 char words[5][32];
-unsigned int wordIndex = 0;
-int prevWordIndex = -1;
+unsigned short wordIndex = 0;
+short prevWordIndex = -1;
 char phrase[32];
 
 boolean registeredPress = false;
 boolean pressed = false;
 boolean buttonA, buttonB, buttonC;
 boolean gaveTeamPoint = false;
-unsigned int pointTotal = 0;
+byte pointTotal = 0;
 
-int state = 0; // 0 = start, 1 = playing, 2 = select team, 3 = done
-int prevState = -1;
+short state = 0; // 0 = start, 1 = playing, 2 = select team, 3 = done
+short prevState = -1;
 unsigned long roundStart = 0;
-unsigned int Apoints, Bpoints;
-unsigned int redraws = 0;
-unsigned int filePosition;
+byte Apoints, Bpoints;
+byte redraws = 0;
+unsigned long filePosition;
+int notelength = 60;
 
 byte smiley[8] = {
   B00000,
@@ -37,7 +38,7 @@ byte smiley[8] = {
 };
 
 void setup() {
-  randomSeed(analogRead(10)); // pin 10 is free
+  randomSeed(analogRead(10)); // pin 10 is free, so see using that
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   lcd.createChar(0, smiley);
@@ -59,9 +60,7 @@ void setup() {
     // don't do anything more:
     return;
   }
-  Serial.println("Init log");
   reread();
-  Serial.println("done");
   roundStart = millis();
   Apoints = Bpoints = 0;
 }
@@ -97,24 +96,31 @@ void loop() {
   if (state != prevState || pressed || state == 1 || redraws > 0) {
     prevState = state;
     if (redraws > 0) redraws--;
-    Serial.println(redraws);
+    //Serial.println(redraws);
     switch (state) {
       case 0: // start
         lcd.clear();
-        lcd.print("Press any button");
+        lcd.print("WordAround v1b ");
+        lcd.write(byte(0));
         lcd.setCursor(0,1);
-        lcd.print("to start. ");
-        //lcd.write(byte(0));
+        lcd.print("Press ");
         lcd.write(0b01111110);
-        if (pressed) {
+        lcd.print(" button!");
+        if (buttonC && pressed) {
+          prevWordIndex = -1;
+          reread();
           state = 1;
           roundStart = millis();
+          Apoints = Bpoints = pointTotal = 0;
+          gaveTeamPoint = false;
+        } else if (buttonA && buttonB && pressed) {
+          state = 4;
         }
         break;
       case 1: // playing
         if ((millis() - roundStart)/1000 > roundLength) {
-          state = 2;
-          playNote(NOTE_FS5, 1000);
+          state = 2; // time's out!
+          playNote(NOTE_C2, 1000);
         } else {
           /* Ticks:
           until 50%: every second
@@ -125,13 +131,13 @@ void loop() {
           int elapsed = (millis() - roundStart)/10, // round off to 10s
               elapsedP = (millis() - roundStart)/10/roundLength; // as percentage
           if (elapsedP < 50 && elapsed % 100 == 0) {
-            playNote(NOTE_C4, 20);
+            playNote(NOTE_C4, 10);
           } else if (elapsedP >= 50 && elapsedP < 70 && elapsed % 50 == 0) {
-            playNote(NOTE_C4, 20);
+            playNote(NOTE_C4, 10);
           } else if (elapsedP >= 70 && elapsedP < 90 && elapsed % 20 == 0) {
-            playNote(NOTE_C4, 20);
+            playNote(NOTE_C4, 10);
           } else if (elapsedP >= 90 && elapsed % 10 == 0) {
-            playNote(NOTE_C4, 20);
+            playNote(NOTE_C4, 10);
           }
         }
         if (wordIndex != prevWordIndex) {
@@ -190,6 +196,7 @@ void loop() {
             roundStart = millis();
             gaveTeamPoint = false;
             state = 1; // another round
+            reread();
             redraws+=2;
           }
           lcd.clear();
@@ -210,10 +217,10 @@ void loop() {
             }
             pointTotal = Apoints + Bpoints;
             gaveTeamPoint = true;
-            if (Apoints > pointsToWin || Bpoints > pointsToWin) {
+            if (Apoints > pointsToWin-1 || Bpoints > pointsToWin-1) {
               state = 3; // someone won
             }
-            redraws+=2;
+            redraws++;
           } 
           lcd.clear();
           lcd.print("Round over.");
@@ -230,18 +237,40 @@ void loop() {
         lcd.clear();
         lcd.print("Game over.");
         lcd.setCursor(0,1);
-        if (Apoints > pointsToWin) {
+        if (Apoints > pointsToWin-1) {
           lcd.print("Team A won!");
-        } else if (Bpoints > pointsToWin) {
+        } else if (Bpoints > pointsToWin-1) {
           lcd.print("Team B won!");
         }
-        // play win sound
         if (buttonC) { // reset game
-          roundStart = millis();
-          Apoints = Bpoints = 0;
-          pointTotal = 0;
-          state = 1;
+          state = 0;
+        } else {
+          // play win sound
+          for (int i = 0; i < 2; i++) {
+            playNote(NOTE_C4, notelength);
+            delay(notelength*3);
+            playNote(NOTE_E4, notelength);
+            delay(notelength*3);
+            playNote(NOTE_G4, notelength);
+            delay(notelength*3);
+            playNote(NOTE_C5, notelength);
+            delay(notelength*3);
+          }
+          playNote(NOTE_E5, notelength);
+          delay(notelength);
+          playNote(NOTE_G5, notelength);
+          delay(notelength);
+          playNote(NOTE_C6, notelength);
+          delay(notelength);
         }
+        break;
+      case 4: // easteregg
+        lcd.clear();
+        lcd.print("ey lamo");
+        if (buttonC) {
+          state = 0;
+        }
+        break;
       default:
         // nothing here
       break;
@@ -254,37 +283,39 @@ void reread() {
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
   File dataFile = SD.open("words.txt");
-  boolean hasStarted = false;
   // if the file is available, write to it:
   if (dataFile) {
     for (int i = 0; i < sizeof(words) / sizeof(char[32]); i++) {
       for (int j = 0; j < 32; j++) {
         words[i][j] = '\0';
       }
-    }
-    char c;
-    for (int i = 0; i < sizeof(words) / sizeof(char[32]); i++) {
-      filePosition = (int)random(
-        dataFile.size()-(
-          35*sizeof(words) / sizeof(char[32])
-        )
-      );
+      filePosition = random(dataFile.size());
+      char c;
       dataFile.seek(filePosition);
-      while (dataFile.available() && dataFile.peek() != '\n' && filePosition > 0) {
+      while (dataFile.peek() != '\n' && filePosition > 0) {
         dataFile.seek(filePosition--);
       }
-      for (int j = 0; j < 32; j++) {
-        if (dataFile.available()) {
-          c = dataFile.read();
-          if (c != '\n' && c != '\r') {
+      c = dataFile.read();
+      while (dataFile.available() && (c == '\n' || c == '\r')) {
+        c = dataFile.read();
+      }
+      if (dataFile.position() == 2) {
+        dataFile.seek(dataFile.position()-2);
+      } else {
+        dataFile.seek(dataFile.position()-1);
+      }
+      int j = 0;
+      while (dataFile.available() && j < 32) {
+        c = dataFile.read();
+        if (c == '\n') {
+          j = 32;
+        } else {
+          if (c != '\r') {
             words[i][j] = c;
-            Serial.print(c);
-          } else {
-            Serial.print('\n');
+            j++;
           }
         }
       }
-      //Serial.println(words[i]);
     }
     dataFile.close();
   } else {
@@ -292,10 +323,6 @@ void reread() {
     lcd.print("Failed to read");
     lcd.setCursor(0,1);
     lcd.print("words.txt");
-  }
-  // randomize the words... or not
-  if (!hasStarted) {
-    reread();
   }
 }
 
